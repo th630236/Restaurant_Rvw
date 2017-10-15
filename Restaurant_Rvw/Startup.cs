647380fd1e87efcf4fa3e4d72b9275c01207205a
Timothy M Hitchcock <timh412@inebraska.com>
2017-10-15 04:19:18 -0500

    Add project files.

---
+++ Restaurant_Rvw/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Restaurant_Rvw.Startup))]
+namespace Restaurant_Rvw
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
